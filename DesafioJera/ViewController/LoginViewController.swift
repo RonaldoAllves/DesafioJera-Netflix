8b1c7d86846d07a1f88c5989945c66237fb5dd3e
Ronaldo Allves <ronaldoalvesdebritopereira@Mac-mini-de-Ronaldo.local>
2021-04-22 20:00:06 -0300

    Criado o login do usuario

--- DesafioJera/ViewController/LoginViewController.swift
+++ DesafioJera/ViewController/LoginViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var campoEmail: UITextField!
+    @IBOutlet weak var campoSenha: UITextField!
+    var auth: Auth!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        auth = Auth.auth()
 
     }
     
@@ -18,5 +25,31 @@ class LoginViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(true, animated: true) //Esconde a barra de navegação
     }
+    
+    @IBAction func logar(_ sender: Any) {
+        
+        //Verificar se os campos foram preenchidos
+        if let email = campoEmail.text{
+            if let senha = campoSenha.text{
+                
+                auth.signIn(withEmail: email, password: senha) { (usuario, erro) in
+                    if erro == nil{
+                        if let usuarioLogado = usuario{
+                            print("Sucesso ao logar usuario:  \(String(describing: usuarioLogado.user.email))")
+                        }
+                    }else{
+                        print("Erro ao logar usuario!")
+                    }
+                }
+                
+            }else{
+                //Criar alerta para campo senha errado
+            }
+        }else{
+            //Criar alerta para campo email errado
+        }
+        
+    }
+    
 
 }
