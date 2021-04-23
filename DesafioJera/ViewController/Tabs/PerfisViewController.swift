52758eef54ce40b26b559aa1fc08117a7476008a
Ronaldo Allves <ronaldoalvesdebritopereira@Mac-mini-de-Ronaldo.local>
2021-04-23 15:37:27 -0300

    Salva imagem do usuario

--- DesafioJera/ViewController/Tabs/PerfisViewController.swift
+++ DesafioJera/ViewController/Tabs/PerfisViewController.swift
@@ -7,15 +7,41 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 
-class PerfisViewController: UIViewController {
+class PerfisViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var auth: Auth!
-
+    var storage: Storage!
+    var imagePicher = UIImagePickerController()
+    
+    @IBOutlet weak var campoNomeLogin: UILabel!
+    @IBOutlet weak var campoEmailLogin: UILabel!
+    @IBOutlet weak var imagemBotaoLogin: UIButton!
+    
+    
+    
+    @IBOutlet weak var nomePerfil1: UITextView!
+    @IBAction func escolherImagemPerfil1(_ sender: Any) {
+        
+        
+        
+    }
+    @IBAction func entrarPerfil1(_ sender: Any) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         auth = Auth.auth()
+        storage = Storage.storage()
+        
+        imagePicher.delegate = self
+        
+        
 
     }
     
@@ -30,5 +56,42 @@ class PerfisViewController: UIViewController {
         
     }
     
+    
+    @IBAction func escolherImagemLogin(_ sender: Any) {
+        
+        imagePicher.sourceType = .savedPhotosAlbum
+        present(imagePicher, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let imagemRecuperada = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        let imagens = storage.reference().child("imagens")
+        
+        if let imagemUpload = imagemRecuperada.jpegData(compressionQuality: 0.3){
+            
+            if let usuarioLogado = auth.currentUser{
+                let idUsuario = usuarioLogado.uid
+                let nomeImagem = "\(idUsuario).jpg"
+                
+                imagens.child("imagemUsuario").child(nomeImagem)
+                    .putData(imagemUpload, metadata: nil) { (metaData, erro) in
+                        
+                        if erro != nil{
+                            //Criar um alerta para erro ao fazer upload da imagem do usuario
+                        }
+                        
+                    }
+            }
+        }
+        
+        self.imagemBotaoLogin.setImage(imagemRecuperada, for: .normal)
+        
+        imagePicher.dismiss(animated: true, completion: nil) //fecha a selecao de imagem ao clicar em alguma foto
+    }
 
 }
